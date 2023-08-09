bb9b00d6731820a7a340aaa7e34bcdd759d8c417
Rocketbro135 <97068196+Rocketbro135@users.noreply.github.com>
2023-08-08 19:30:00 -0500

    Update embeds.py

--- cogs/embeds/File_embeds/embeds.py
+++ cogs/embeds/File_embeds/embeds.py
@@ -1,4 +1,5 @@
 import discord
+from cogs.Command_Settings.bot_settings import bot_image_thumbnail_url as thumbnail_image
 
 invalidEmbed = discord.Embed(
                 title="Error: Invalid game mode.\ntype /gamemodes to view all available game modes!",
@@ -63,3 +64,39 @@ disable_dead_role.add_field(
             value="🚫💀This means when someone dies, their role will NOT be revealed to everyone!\n✅📜This also means wills are in play!",
             inline=True
             )
+
+max_players_reached = discord.Embed(
+            title="",
+            description="The max amount of players is reached! Type /queue to see the queue.",
+            color=discord.colour.parse_hex_number('FF0000')
+            )
+max_players_reached.set_author(
+            name="Mafiabot 2.0",
+            icon_url= "https://cdn.wanderer.moe/honkai-star-rail/splash-art/yanqing-full.png"
+            )
+
+not_in_party_embed = discord.Embed(
+                title="",
+                description="",
+                color=discord.Color.red()
+            )
+not_in_party_embed.add_field(
+                name="",
+                value="You are not in the party lol😅",
+                inline=True
+            )
+not_in_party_embed.set_author(name="Mafiabot 2.0")
+
+setup_game_embed = discord.Embed(
+    title="Everything's ready! Everyone feel free to join a voice chat and /start to start the game!",
+    description=" Make sure you understand how the game works! (Info can be found with /game)",
+    color=discord.Color.green()
+)
+setup_game_embed.set_thumbnail(url='https://pbs.twimg.com/media/DWVbyz5WsAA93-y.png')
+
+start_game_embed_Fail = discord.Embed(
+    title="",
+    description="You didn't set up yet. Type /setup first.",
+    color=discord.Color.red()
+)
+start_game_embed_Fail.set_thumbnail(url=thumbnail_image)
